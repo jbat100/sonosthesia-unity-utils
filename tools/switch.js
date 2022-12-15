5af84d764d52760be2aaaa39bef23b2ad776e0e2
Jonathan Thorpe <johnny.frenchy@gmail.com>
2022-12-16 00:57:56 +0100

    Support switch back to packages and relative link

--- tools/switch.js
+++ tools/switch.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const parser = require('args-parser')
 
 let packages = {
     "com.sonosthesia.adaptivemidi" : "sonosthesia-unity-adaptivemidi",
@@ -12,31 +13,82 @@ let packages = {
     "com.sonosthesia.utils" : "sonosthesia-unity-utils"
 }
 
-function switchToLocal(repository, package) {
+function linkPackage(repository, package) {
     let repositoryPath = path.join(__dirname, "..", "..", repository)
     let packagePath = path.join(repositoryPath, package, "package.json")
-    let unityPath = path.join(repositoryPath, "UnityProject", "Packages", "manifest.json")
-    console.log("switchToLocal package " + packagePath + " unity " + unityPath)
-    if (!fs.existsSync(packagePath) || !fs.existsSync(unityPath)) {
+    let manifestPath = path.join(repositoryPath, "UnityProject", "Packages", "manifest.json")
+    console.log("linkPackage " + packagePath + " unity " + manifestPath)
+    if (!fs.existsSync(packagePath) || !fs.existsSync(manifestPath)) {
+        console.log("bailing out...")
+        return
+    }
+    let unityJSON = JSON.parse(fs.readFileSync(manifestPath));
+    unityJSON.dependencies[package] = "file:" + path.relative(
+        path.join(repositoryPath, "UnityProject", "Packages"), 
+        path.join(__dirname, "..", "..", repository, package)
+    ).replaceAll(path.sep, "/"); 
+    console.log(unityJSON)
+    fs.writeFileSync(manifestPath, JSON.stringify(unityJSON, null, 2))
+}
+
+function packageDependencies(repository, package) {
+    let repositoryPath = path.join(__dirname, "..", "..", repository)
+    let packagePath = path.join(repositoryPath, package, "package.json")
+    let manifestPath = path.join(repositoryPath, "UnityProject", "Packages", "manifest.json")
+    console.log("packageDependencies " + packagePath + " unity " + manifestPath)
+    if (!fs.existsSync(packagePath) || !fs.existsSync(manifestPath)) {
         console.log("bailing out...")
         return
     }
     let packageJSON = JSON.parse(fs.readFileSync(packagePath));
-    let unityJSON = JSON.parse(fs.readFileSync(unityPath));
+    let unityJSON = JSON.parse(fs.readFileSync(manifestPath));
     for (const [dependency, version] of Object.entries(packageJSON.dependencies)) {
         if (!dependency.startsWith("com.sonosthesia")) {
             continue
         }
         console.log("processing " + dependency)
-        unityJSON.dependencies[dependency] = "file:" + path.join(__dirname, "..", "..", packages[dependency], dependency)
+        delete unityJSON.dependencies[dependency]
     }
     console.log(unityJSON)
-    fs.writeFileSync(unityPath, JSON.stringify(unityJSON, null, 2))
+    fs.writeFileSync(manifestPath, JSON.stringify(unityJSON, null, 2))
+}
+
+function localDependencies(repository, package) {
+    let repositoryPath = path.join(__dirname, "..", "..", repository)
+    let packagePath = path.join(repositoryPath, package, "package.json")
+    let manifestPath = path.join(repositoryPath, "UnityProject", "Packages", "manifest.json")
+    console.log("localDependencies " + packagePath + " unity " + manifestPath)
+    if (!fs.existsSync(packagePath) || !fs.existsSync(manifestPath)) {
+        console.log("bailing out...")
+        return
+    }
+    let packageJSON = JSON.parse(fs.readFileSync(packagePath));
+    let unityJSON = JSON.parse(fs.readFileSync(manifestPath));
+    for (const [dependency, version] of Object.entries(packageJSON.dependencies)) {
+        if (!dependency.startsWith("com.sonosthesia")) {
+            continue
+        }
+        console.log("processing " + dependency)
+        // tried to use path.posix.relative but things get messy
+        unityJSON.dependencies[dependency] = "file:" + path.relative(
+            path.join(repositoryPath, "UnityProject", "Packages"), 
+            path.join(__dirname, "..", "..", packages[dependency], dependency)
+        ).replaceAll(path.sep, "/"); 
+    }
+    console.log(unityJSON)
+    fs.writeFileSync(manifestPath, JSON.stringify(unityJSON, null, 2))
 }
 
 function run() {
+    let args = parser(process.argv)
     for (const [package, repository] of Object.entries(packages)) {
-        switchToLocal(repository, package);
+        if (args.local) {
+            localDependencies(repository, package);
+        } else if (args.package) {
+            packageDependencies(repository, package)
+        } else if (args.link) {
+            linkPackage(repository, package)
+        }
     }
 }
 
